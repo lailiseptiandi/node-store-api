132b74ac7f1a6ceabd28bbc64f05b109beb8e90b
Laili Septiandi <lailiseptiandi2@gmail.com>
2023-09-07 14:56:04 +0700

    add. helper response success && error

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,6 +1,26 @@
-const MyHelper = require('./helper');
+const Helper = require('./helper');
 
+
+function sendSuccessResponse(res, message, data = null) {
+    res.status(200).json({
+        success: true,
+        message: message,
+        data: data,
+    });
+}
+
+
+function sendErrorResponse(res, message, code = 400){
+      res.status(code).json({
+        success: false,
+        message: message,
+        data: null,
+    });
+
+}
 
 module.exports = {
-    MyHelper
+    Helper,
+    sendSuccessResponse,
+    sendErrorResponse
 }
